76ca9471bd398ffb523198a6cfc9898fd4c965fd
Carlos Daniel <51516076+CarlosDaniel0@users.noreply.github.com>
2025-06-15 06:41:35 -0300

    BugFix: unity in supermarket product

--- src/pages/Products/CreateOrUpdate.tsx
+++ src/pages/Products/CreateOrUpdate.tsx
@@ -335,7 +335,7 @@ export default function CreateOrUpdate(
               }
             />
           )}
-          {['lists'].includes(path) && (
+          {['lists', 'supermarkets'].includes(path) && (
             <Text
               id="inpTxtUnity"
               container={{ style: { flexBasis: '40%' } }}
